a154c2b77ba4d294ed683a5d0900ffba05127904
Nick Gerakines <nick@gerakines.net>
2010-05-22 21:35:43 -0700

    Adding google analytics js. Increasing line height

--- otc.rb
+++ otc.rb
@@ -25,7 +25,7 @@ body {
   text-transform: none;
   letter-spacing: 0;
   word-spacing: 0;
-  line-height: 1;
+  line-height: 1.2;
 }
 #content {
   width: 760px;
@@ -43,9 +43,17 @@ body {
 <strong>What times do you raid?</strong>
 We raid in the evenings in 2 hour windows that varies from week to week. We use needtomeet.com to determine when is the best time for the most people to raid and adjust our schedules accordingly.
         </p><p>
-<strong>Is this really your website?</strong> Yeah, we don't need to swing it around because we know its there. We use Google Wave, needtomeet.com, realmfirst.com, Ventrilo and email to communicate and if you join, you will too.
-        </p><p>
 <strong>How do I join?</strong> Hop onto Lightbringer, do a `<i>/who on the clock</i>` and ask someone. They will point you in the right direction.
         </p></div>
+    <script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-660447-8']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+    </script>
     </body> 
 </html> 
