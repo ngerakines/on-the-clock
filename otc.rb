03a25ce95314b5b8e986f012272c58a2d53b2846
Nick Gerakines <nick@gerakines.net>
2010-07-06 17:04:17 -0700

    Adding online application.

--- otc.rb
+++ otc.rb
@@ -1,5 +1,22 @@
 require 'rubygems'
 require 'sinatra'
+require 'pony'
+
+TPMI_SMTP_OPTIONS = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN'],
+}
+
+def send_email(subject, html_body)
+    Pony.mail(:to => 'nick@gerakines.net', :from => 'apply@on-the-clock.us',
+        :subject => subject,
+        :html_body => html_body,
+        :via => :smtp, :via_options => TPMI_SMTP_OPTIONS)
+end
 
 get '/' do
     erb :index
@@ -8,3 +25,18 @@ end
 get '/members' do
     erb :members
 end
+
+get '/apply' do
+    erb :apply
+end
+
+post '/apply' do
+    name = params[:name]
+    @body = ""
+    params.each_key do |key|
+        @body << "#{key} = #{params[key]}\n"
+    end
+    send_email("Guild application from #{name}", @body)
+    erb :thanks
+end
+
